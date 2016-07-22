d617bd73e36f5e71261e2f710cbd1c643fbf3293
Rohan Agrawal <rohan@osrfoundation.org>
2016-07-22 16:19:46 -0700

    better printing

--- joy/joy_node.cpp
+++ joy/joy_node.cpp
@@ -158,16 +158,17 @@ int main(int argc, char * argv[])
         cmd_vel_msg->linear.x = -msg->axes[1] * g_scale_linear;
         cmd_vel_msg->angular.z = -msg->axes[0] * g_scale_angular;
         cmd_vel_pub->publish(cmd_vel_msg);
+        printf("publishing: (%6.3f, %6.3f)\n", cmd_vel_msg->linear.x, cmd_vel_msg->angular.z);
         sent_zero = false;
     }
     else if (!sent_zero) {
         cmd_vel_msg->linear.x = 0;
         cmd_vel_msg->angular.z = 0;
         cmd_vel_pub->publish(cmd_vel_msg);
+        printf("deadman released, stopping robot\n");
         sent_zero = true;
     }
 
-    printf("publishing: (%6.3f, %6.3f)\n", cmd_vel_msg->linear.x, cmd_vel_msg->angular.z);
     joy_pub->publish(msg);
     //loop_rate.sleep();
   }
