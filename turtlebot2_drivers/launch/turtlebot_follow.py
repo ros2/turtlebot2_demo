a5c36bd4535f3e4ef26fdf36ddeace1b8086ffd3
Chris Lalancette <clalancette@openrobotics.org>
2017-06-13 23:17:07 +0000

    Fixes to make TB2 follower demo build/launch.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- turtlebot2_drivers/launch/turtlebot_follow.py
+++ turtlebot2_drivers/launch/turtlebot_follow.py
@@ -18,17 +18,17 @@ from launch.exit_handler import ignore_exit_handler, restart_exit_handler
 def launch(launch_descriptor, argv):
     ld = launch_descriptor
     ld.add_process(
-        cmd=['kobuki_node__rmw_opensplice_cpp'],
+        cmd=['kobuki_node'],
         name='kobuki_node',
         exit_handler=restart_exit_handler,
     )
     ld.add_process(
-        cmd=['astra_camera_node__rmw_opensplice_cpp'],
+        cmd=['astra_camera_node', '-C', '-I', '-dw', '320', '-dh', '240'],
         name='astra_camera_node',
         exit_handler=restart_exit_handler,
     )
     ld.add_process(
-        cmd=['follower__rmw_opensplice_cpp'],
+        cmd=['follower'],
         name='follower',
         exit_handler=restart_exit_handler,
     )
