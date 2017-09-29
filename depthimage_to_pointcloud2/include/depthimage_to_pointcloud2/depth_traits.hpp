921af9977788b14bc2b9816a0e3c378736bb8d0d
Mikael Arguedas <mikael@osrfoundation.org>
2017-09-29 07:51:00 -0700

    update style to match latest uncrustify

--- depthimage_to_pointcloud2/include/depthimage_to_pointcloud2/depth_traits.hpp
+++ depthimage_to_pointcloud2/include/depthimage_to_pointcloud2/depth_traits.hpp
@@ -50,18 +50,18 @@ struct DepthTraits {};
 template<>
 struct DepthTraits<uint16_t>
 {
-  static inline bool valid(uint16_t depth) {return depth != 0; }
-  static inline float toMeters(uint16_t depth) {return depth * 0.001f; }  // originally mm
-  static inline uint16_t fromMeters(float depth) {return (depth * 1000.0f) + 0.5f; }
+  static inline bool valid(uint16_t depth) {return depth != 0;}
+  static inline float toMeters(uint16_t depth) {return depth * 0.001f;}   // originally mm
+  static inline uint16_t fromMeters(float depth) {return (depth * 1000.0f) + 0.5f;}
   static inline void initializeBuffer(std::vector<uint8_t> &) {}  // Do nothing
 };
 
 template<>
 struct DepthTraits<float>
 {
-  static inline bool valid(float depth) {return std::isfinite(depth); }
-  static inline float toMeters(float depth) {return depth; }
-  static inline float fromMeters(float depth) {return depth; }
+  static inline bool valid(float depth) {return std::isfinite(depth);}
+  static inline float toMeters(float depth) {return depth;}
+  static inline float fromMeters(float depth) {return depth;}
 
   static inline void initializeBuffer(std::vector<uint8_t> & buffer)
   {
